e3da23ca691f6538d9d210c50dcde81945d4aa27
Ayana Ussenbayeva <164485227+Idyxdoxiwowl@users.noreply.github.com>
2025-05-11 02:36:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
-from torch.nn.functional import sigmoid
 from fastapi.responses import HTMLResponse
 from pathlib import Path
+import joblib
 
-# === Настройки ===
-MODEL_PATH = "bert_toxic_model"  # если в папке
+# === Константы ===
+MODEL_PATH = "logreg_pipeline.pkl"
 LABELS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 THRESHOLD = 0.5
 
-# === Загрузка модели и токенизатора ===
-tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
-model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
-model.eval()
+# === Загрузка модели ===
+pipeline = joblib.load(MODEL_PATH)
 
 # === FastAPI ===
 app = FastAPI()
@@ -24,18 +20,15 @@ class InputText(BaseModel):
 
 @app.post("/predict")
 def predict(input: InputText):
-    with torch.no_grad():
-        tokens = tokenizer(input.text, return_tensors="pt", truncation=True, padding=True, max_length=128)
-        logits = model(**tokens).logits
-        probs = sigmoid(logits).squeeze().tolist()
+    # Прогноз вероятностей
+    probs = pipeline.predict_proba([input.text])
+    
+    result = {
+        LABELS[i]: bool(probs[0][i] > THRESHOLD)
+        for i in range(len(LABELS))
+    }
+    return result
 
-        result = {
-            LABELS[i]: bool(probs[i] > THRESHOLD)
-            for i in range(len(LABELS))
-        }
-        return result
-
-# === Фронтенд ===
 @app.get("/", response_class=HTMLResponse)
 def serve_index():
     return Path("index.html").read_text(encoding="utf-8")
